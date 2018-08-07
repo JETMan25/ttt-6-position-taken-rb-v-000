1dc7ae86faa466d1e0c8da50d36dd24cca6ee535
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-07 13:58:29 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -17,6 +17,12 @@ def move(board, index, value = "X")
   
 end
 
+def position_taken?(board, index)
+ if position_taken?(board, " "} == " "
+ elsif position_taken?(board, index} == ""
+ return "false"
+ 
+end
 
 
 # code your #position_taken? method here!
