01c4493b1f83907951ed2c2451b9c526a59665bf
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-07 14:07:57 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,4 @@
-postion_taken?
 
-def position_taken?(board, index)
-if position_taken?() == "x"
-elsif position_taken?() == "O"
-return "true"
-end
- 
 
 
 # code your #position_taken? method here!
