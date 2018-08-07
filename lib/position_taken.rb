b07801bce4a189cc085f0738f8781fa666b514fe
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-08-07 14:12:25 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,10 @@
-array[index_number]
+array[#{index_number}]
 
+def position_taken?(board, index)
+  if position_taken?([#{board}, #{index}]) == " "
+  elsif position_taken?([#board}, #{index}]) == ""
+  return "false"
+end
 
 
 # code your #position_taken? method here!
